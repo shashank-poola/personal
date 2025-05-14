c8461767b226781f0c3d3a6f116624fd2f324032
shashank-poola <shashankpoola123@gmail.com>
2025-05-14 10:21:31 +0530

    changed commits

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 
 export default function PersonalWebsite() {
-<<<<<<< HEAD
-
-  return (
-    <div className="w-full min-h-screen bg-black text-white font-[Space+Grotesk] flex items-center justify-center ">
-      <div className="flex justify-center">
-      <div className="px-4 py-12 w-full max-w-4xl">
-=======
   return (
     <div className="w-full min-h-screen bg-black text-white font-['Bricolage_Grotesque',_sans-serif] flex items-center justify-center ">
       <div className="flex justify-center">
@@ -16,58 +9,11 @@ export default function PersonalWebsite() {
         <header className="mb-16 mt-8">
           <h1 className="text-5xl font-bold mb-12">Shashank Poola</h1>
         </header>
->>>>>>> 3a9045d92656d8448bdb64f62edcd2cf8a10cc13
 
         {/* Main Content */}
         <main>
           <div className="flex flex-col md:flex-row justify-center items-start gap-12 font-bricolage">
             <div className="md:w-2/3 mb-12">
-<<<<<<< HEAD
-                <h1 className="text-5xl font-bold mb-12">Shashank Poola</h1>
-              <p> I'm a 21-year-old <a href="#" className="italic">full-stack developer</a> from India, 
-              diving into the crypto space and Web3. Building Unique websites like
-              Tipfinity, a platform where fans can pay creators with crypto using solana.
-              </p>
-              <br/>
-              <p className="text-lg mb-12 font-bricolage">
-                Location: {'\uD83D\uDCCD'}Hyderabad, India
-              </p>
-              <br/> 
-
-              <div className="md:w-2/3 mb-12">
-                    <h3 className="text-2xl font-bold mb-12">Skills:-</h3>
-              <p className="text-lg">
-                  React.js -- Node.js -- JavaScript -- TypeScript -- Tailwind CSS -- GSAP -- Express -- MongoDB -- Git -- Github -- Vercel
-              </p>
-              </div>
-
-              <br/>
-
-              <div className="md:w-2/3 mb-12">
-                    <h3 className="text-2xl font-bold mb-12">Proof of Work:-</h3>
-                    <h5 className="text-xl mb-12">
-                      <a href="https://nexora.shasha.ink" className="italic">Nexora</a> -  Nexora is a modern, animated scroll website built with GSAP for smooth, engaging visuals.
-                    </h5>
-
-                    <h5 className="text-xl mb-12">
-                      <a href="https://nexora.shasha.ink" className="italic">movieflix</a> -  Your go-to movie discovery platform! Explore trending movies, get inspired by what people are searching, and enjoy movies seamless.
-                    </h5>
-
-                    <h5 className="text-xl mb-12">
-                      <a href="https://github.com/shashank-poola/OpenNote" className="italic">OpenNote</a> -  A minimal and user-friendly task management app to add, edit, and delete your daily tasks. Built with clean UI and real-time updates for better productivity.
-                    </h5>
-              </div>
-
-
-              <ul className="space-y-4">
-                <li className="flex items-center">
-                  <span className="text-gray-400 mr-2">•</span>
-                  <span>X - <a href="https://x.com/shashankpoola" className="text-white underline">@shashankpoola</a></span>
-                </li>
-                <li className="flex items-center">
-                  <span className="text-gray-400 mr-2">•</span>
-                  <span>Linkedin - <a href="https://linkedin.com/in/shashank-poola" className="text-white underline">@shashank-poola</a></span>
-=======
               <p className="text-lg mb-6">
               I'm a 21-year-old <a href="#" className="">full-stack developer</a> from India, 
               diving into the crypto space and Web3. I'm building a web-based wallet and 
@@ -88,7 +34,6 @@ export default function PersonalWebsite() {
                 <li className="flex items-center">
                   <span className="text-gray-400 mr-2">•</span>
                   <span>Instagram - <a href="https://instagram.com/shashankpoola" className="text-white underline">@mr___shashank__</a></span>
->>>>>>> 3a9045d92656d8448bdb64f62edcd2cf8a10cc13
                 </li>
                 <li className="flex items-center">
                   <span className="text-gray-400 mr-2">•</span>
@@ -96,11 +41,7 @@ export default function PersonalWebsite() {
                 </li>
                 <li className="flex items-center">
                   <span className="text-gray-400 mr-2">•</span>
-<<<<<<< HEAD
-                  <span>Email - <a href="mailto:hi@shasha.ink" className="text-white underline">hi@shasha.ink</a></span>
-=======
                   <span>Email - <a href="mailto:hi@theshashank.xyz" className="text-white underline">hi@theshashank.xyz</a></span>
->>>>>>> 3a9045d92656d8448bdb64f62edcd2cf8a10cc13
                 </li>
               </ul>
             </div>
@@ -109,11 +50,7 @@ export default function PersonalWebsite() {
 
         {/* Footer */}
         <footer className="mt-32 mb-4 text-center text-sm text-gray-500">
-<<<<<<< HEAD
-          <p>2025 © shasha.ink</p>
-=======
           <p>2025 © theshashank.</p>
->>>>>>> 3a9045d92656d8448bdb64f62edcd2cf8a10cc13
         </footer>
       </div>
     </div>
